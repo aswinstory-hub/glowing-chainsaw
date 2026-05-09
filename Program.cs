235b54423f5f556c07bce09329bfb78b7e893088
Aswin <aswin.story@gmail.com>
2026-05-09 14:35:43 +0530

    Add simple Raylib framework

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+Raylib.InitWindow(1280, 720, "My First Raylib Game");
+
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.Black);
+
+    Raylib.DrawText("HELLO RAYLIB", 250, 200, 30, Color.Green);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
